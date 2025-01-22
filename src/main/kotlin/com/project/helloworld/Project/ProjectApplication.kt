b4bd9f0b7a86e725615466c176363b441c09c929
OnedevMaster <126522151+OnedevMaster@users.noreply.github.com>
2025-01-22 17:30:55 -0300

    Update ProjectApplication.kt

--- src/main/kotlin/com/project/helloworld/Project/ProjectApplication.kt
+++ src/main/kotlin/com/project/helloworld/Project/ProjectApplication.kt
@@ -2,10 +2,20 @@ package com.project.helloworld.Project
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ProjectApplication
 
 fun main(args: Array<String>) {
 	runApplication<ProjectApplication>(*args)
+}
+
+@RestController
+class DemoController{
+    @GetMapping("/")
+    fun hello() : String {
+        return "Hello World" 
+    }	
 }
